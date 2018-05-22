58142cbd231f3d076a610b38a706a8f57a2b3d16
malhusse <malhusse@fnal.gov>
2018-05-22 15:50:26 -0500

    folder with environment and cmake configs

---
+++ config/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#	for CMSSW use it's the CMSSW/src/Analysis
+#	for STANDALONE use it's the build directory or for testing purposes repo
+export ANALYSISHOME=/afs/cern.ch/user/m/malhusse/h2mu/CMSSW_9_4_5/src/Analysis/
+#export PYTHONPATH=$ANALYSISHOME:$PYTHONPATH
+echo "ANALYSIS HOME is now at $ANALYSISHOME"
+#echo $PYTHONPATH
