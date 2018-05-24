a6f6227e3d2cabd77917dceb85adcada5ff9fa30
malhusse <malhusse@fnal.gov>
2018-05-24 17:22:47 +0200

    fix environment path, remove QIE* classes

--- config/env.sh
+++ config/env.sh
@@ -2,7 +2,7 @@
 
 #	for CMSSW use it's the CMSSW/src/Analysis
 #	for STANDALONE use it's the build directory or for testing purposes repo
-export ANALYSISHOME=/afs/cern.ch/user/m/malhusse/h2mu/CMSSW_9_4_5/src/Analysis/
+export ANALYSISHOME=/afs/cern.ch/user/m/malhusse/work/CMSSW_9_4_0/src/HMuMu/
 #export PYTHONPATH=$ANALYSISHOME:$PYTHONPATH
 echo "ANALYSIS HOME is now at $ANALYSISHOME"
 #echo $PYTHONPATH
