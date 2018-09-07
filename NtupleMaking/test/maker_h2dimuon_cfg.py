b512e00621f1c32e5c5f7a7cf48d07de7112fd47
malhusse <malhusse@fnal.gov>
2018-09-08 00:19:31 +0200

    updated ntupler test script

--- NtupleMaking/test/maker_h2dimuon_cfg.py
+++ NtupleMaking/test/maker_h2dimuon_cfg.py
@@ -20,43 +20,44 @@ if "ANALYSISHOME" not in os.environ.keys():
     raise NameError("Can not find ANALYSISHOME env var")
 sys.path.append(os.path.join(os.environ["ANALYSISHOME"], "Configuration", "higgs"))
 import Samples as S
-import Dataset as DS
+#import Dataset as DS
 
-data_datasets = S.datadatasets
+#data_datasets = S.datadatasets
 #mc_datasets = Samples.mcMoriond2017datasets
 jsonfiles = S.jsonfiles
-print(data_datasets)
+#print(data_datasets)
 
 jsontag = "2017_Synch"
 jsonfile = jsonfiles[jsontag]
-dataset = None
-dataset = data_datasets["/SingleMuon/Run2017B-17Nov2017-v1/MINIAOD"]
-print(dataset)
+#dataset = None
+#dataset = data_datasets["/SingleMuon/Run2017B-17Nov2017-v1/MINIAOD"]
+#print(dataset)
 
-if dataset == None:
-    print("-" * 40)
-    print("dataset is None")
-    print("-" * 40)
-    sys.exit(1)
+#if dataset == None:
+#    print("-" * 40)
+#    print("dataset is None")
+#    print("-" * 40)
+#    sys.exit(1)
 
-ntuple = DS.Ntuple(
-    dataset,
-    json="json/" + jsonfile.filename,
-    storage=None,
-    rootpath=None,
-    timestamp=None,
-    cmssw="94X")
+#ntuple = DS.Ntuple(
+#    dataset,
+json= "json/" + jsonfile.filename
+#    storage=None,
+#    rootpath=None,
+#    timestamp=None,
+#    cmssw="94X")
 
 #
 #   a few settings
 #
-thisIsData = ntuple.isData
-globalTag = ntuple.globaltag
+#thisIsData = ntuple.isData
+thisIsData = True
+#globalTag = ntuple.globaltag
+globalTag = '94X_dataRun2_v10'
+#readFiles = cms.untracked.vstring()
+#readFiles.extend(open(("sample_file_lists/%s/" % ("data" if ntuple.isData else "mc"))+ntuple.test_file).read().splitlines())
 
-readFiles = cms.untracked.vstring()
-readFiles.extend(open(("sample_file_lists/%s/" % ("data" if ntuple.isData else "mc"))+ntuple.test_file).read().splitlines())
-
-print(readFiles)
+#print(readFiles)
 #
 #   Differentiate between DATA and MC
 #
@@ -80,20 +81,39 @@ if thisIsData:
 else:
     print('Running over MC sample')
 
-print("Sample Name:    " + ntuple.name)
+#print("Sample Name:    " + ntuple.name)
 print("")
 print("")
+
+## JET ENERGY CORRECTIONS
+
+from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
+
+updateJetCollection(
+   process,
+   jetSource = cms.InputTag('slimmedJets'),
+   labelName = 'UpdatedJEC',
+   jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual']), 'None')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
+)
+
+
 # #
 # #   Pool Source with proper LSs
 # #
-process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(100))
-process.source = cms.Source("PoolSource", fileNames=readFiles)
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(-1))
+
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+#'file:DE1721FC-10D9-E711-B475-0025907B4F04.root'))
+'file:/eos/cms/store/user/amarini/Sync/0E555487-7241-E811-9209-002481CFC92C.root'))
+
+#readFiles)
 process.options = cms.untracked.PSet(wantSummary=cms.untracked.bool(False))
 process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
 if thisIsData:
     import FWCore.PythonUtilities.LumiList as LumiList
     process.source.lumisToProcess = LumiList.LumiList(
-        filename=ntuple.json).getVLuminosityBlockRange()
+#        filename=ntuple.json).getVLuminosityBlockRange()
+         filename=json).getVLuminosityBlockRange()
 
 # #
 # #  Electron ID Setup - cut based
@@ -108,8 +128,10 @@ for idmod in my_id_modules:
     setupAllVIDIdsInModule(process, idmod, setupVIDElectronSelection)
 
 process.TFileService = cms.Service("TFileService", fileName=cms.string("ntuples.root"))
-process.p = cms.Path(process.egmGsfElectronIDSequence * process.ntuplemaker_H2DiMuonMaker)
+process.jecSequence = cms.Sequence(process.patJetCorrFactorsUpdatedJEC * process.updatedPatJetsUpdatedJEC)
+process.p = cms.Path(process.egmGsfElectronIDSequence * process.jecSequence * process.ntuplemaker_H2DiMuonMaker)
 
-process.out = cms.OutputModule(
-    "PoolOutputModule", fileName=cms.untracked.string("test.root"))
-process.finalize = cms.EndPath(process.out)
+# this outputs the original file with all info!
+#process.out = cms.OutputModule(
+#    "PoolOutputModule", fileName=cms.untracked.string("test.root"))
+#process.finalize = cms.EndPath(process.out)
