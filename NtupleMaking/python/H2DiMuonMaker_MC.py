b1d41c992a51b47a6ebd1d099fc0cd83e7a1ddc4
malhusse <malhusse@fnal.gov>
2018-09-17 12:52:48 -0500

    update MC pset for 2017

--- NtupleMaking/python/H2DiMuonMaker_MC.py
+++ NtupleMaking/python/H2DiMuonMaker_MC.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 pt = 27
 
-name = "H2DiMuonMaker_NoPairing"
-hlt = "HLT"
+name = "H2DiMuonMaker"
 
 ntuplemaker_H2DiMuonMaker = cms.EDAnalyzer(
     name,
@@ -12,14 +11,16 @@ ntuplemaker_H2DiMuonMaker = cms.EDAnalyzer(
     tagTaus=cms.untracked.InputTag("slimmedTaus"),
     tagPV=cms.untracked.InputTag("offlineSlimmedPrimaryVertices"),
     tagBS=cms.untracked.InputTag("offlineBeamSpot"),
-    tagPrunedGenParticles=cms.untracked.InputTag("prunedGenParticles"),
-    tagPackedGenParticles=cms.untracked.InputTag("packedGenParticles"),
-    tagTriggerResults=cms.untracked.InputTag("TriggerResults", "", hlt),
+    # tagPrunedGenParticles=cms.untracked.InputTag("prunedGenParticles"),
+    # tagPackedGenParticles=cms.untracked.InputTag("packedGenParticles"),
+    tagTriggerResults=cms.untracked.InputTag("TriggerResults", "", "HLT"),
     tagTriggerObjects=cms.untracked.InputTag("slimmedPatTrigger"),
+    tagMetFilterResults=cms.untracked.InputTag("TriggerResults", "", "RECO"),
     tagMET=cms.untracked.InputTag("slimmedMETs"),
-    tagPFJets=cms.untracked.InputTag("slimmedJets"),
+    tagJets=cms.untracked.InputTag("updatedPatJetsUpdatedJEC"),
     tagGenJets=cms.untracked.InputTag("slimmedGenJets"),
     tagConversions=cms.untracked.InputTag("reducedEgamma:reducedConversions"),
+    tagLHE=cms.untracked.InputTag("externalLHEProducer"),
 
     # electron cut based id
     tagElectronCutBasedId_veto=cms.untracked.InputTag(
@@ -30,19 +31,28 @@ ntuplemaker_H2DiMuonMaker = cms.EDAnalyzer(
         "egmGsfElectronIDs:cutBasedElectronID-Fall17-94X-V1-medium"),
     tagElectronCutBasedId_tight=cms.untracked.InputTag(
         "egmGsfElectronIDs:cutBasedElectronID-Fall17-94X-V1-tight"),
+
     #
     #	Meta Data
     #
     checkTrigger=cms.untracked.bool(True),
     isMC=cms.untracked.bool(True),
-    triggerNames=cms.untracked.vstring("HLT_IsoMu%d" % pt, "HLT_IsoTkMu%d" % pt),
+    triggerNames=cms.untracked.vstring(
+        "HLT_IsoMu%d" % pt, "HLT_IsoTkMu%d" % pt),
+    metFilterNames=cms.untracked.vstring(["Flag_goodVertices",
+                                          # "Flag_globalSuperTightHalo2016Filter", NOT SUGGESTED
+                                          "Flag_HBHENoiseFilter", "Flag_HBHENoiseIsoFilter",
+                                          "Flag_EcalDeadCellTriggerPrimitiveFilter", "Flag_BadPFMuonFilter", "Flag_BadChargedCandidateFilter"]),
+    #   , "Flag_eeBadScFilter"]), NOT SUGGESTED
+    
     nMuons=cms.untracked.int32(2),
     isGlobalMuon=cms.untracked.bool(True),
     isStandAloneMuon=cms.untracked.bool(False),
     isTrackerMuon=cms.untracked.bool(True),
-    minPt=cms.untracked.double(10),
+    minPt=cms.untracked.double(20),
     maxeta=cms.untracked.double(2.4),
-    btagNames=cms.untracked.vstring(["pfDeepCSVJetTags:probb", "pfDeepCSVJetTags:probbb"]),
+    btagNames=cms.untracked.vstring(
+        ["pfDeepCSVJetTags:probb", "pfDeepCSVJetTags:probbb"]),
     tauIDNames=cms.untracked.vstring([""]),
 
     #
