9d0c9622dd0f8a5ca00a2ceb8260dfdef9523882
malhusse <malhusse@fnal.gov>
2019-05-13 16:48:24 -0500

    new generation

--- generate_list.py
+++ generate_list.py
@@ -1,3 +1,5 @@
+year = "2018"
+
 import os
 import sys
 sys.path.append(os.path.join(
@@ -5,35 +7,58 @@ os.environ["ANALYSISHOME"], "Configuration", "higgs"))
 import Samples as S
 import Dataset as DS
 
-data_datasets = S.data_2017
+data_datasets_dic = {
+    "2016": S.data_2016,
+    "2017": S.data_2017,
+    "2018": S.data_2018
+}
+
+cmssw_dic = {
+    "2016": "94X",
+    "2017": "94X",
+    "2018": "102X"
+}
 
 jsonfiles = S.jsonfiles
-jsontag = "EOY2017ReReco"
-jsonfile = jsonfiles[jsontag]
-cmssw = "94X"
+jsonfile = jsonfiles[year]
+
+cmssw = cmssw_dic[year]
+
 storage = "EOS"
-rootpath = "/store/user/malhusse/higgs_ntuples/2017"
+rootpath = "/store/user/malhusse/higgs_ntuples/final/" + year
+print(rootpath)
+
 data_ntuples = []
 aux = "Mu27"
-filelistdir = "/uscms/home/malhusse/nobackup/build/AnalysisCode/filelists"
 
-for d in data_datasets:
-    ntuple = DS.Ntuple(data_datasets[d],
+filelistdir = "/uscms_data/d3/malhusse/analysis/AnalysisCode/filelists/" + year
+
+for d, v in data_datasets_dic[year].items():
+    # print(d)
+    # print(v.label)
+    # print(v.aux)
+    ntuple = DS.Ntuple(v,
                        json = jsonfile.filename,
                        cmssw = cmssw,
-                       storage = "EOS",
+                       storage = storage,
                        rootpath = os.path.join(rootpath, "data"),
                        timestamp = None,
                        aux = aux
                        )
     data_ntuples.append(ntuple)
-print data_ntuples
+# print data_ntuples
+
+timestamps = []
 
 for ntuple in data_ntuples:
     try:
-        filelist_list = S.discoverFileList(ntuple)
-        #print filelist_list
-        filelist = os.path.join(filelistdir, S.buildFileListName(ntuple))
+        for timestamp in S.buildTimeStamp(ntuple):
+            if timestamp in timestamps:
+                continue
+            else:
+                timestamps.append(timestamp)
+                filelist_list = S.discoverFileList(ntuple, timestamp)
+                filelist = os.path.join(filelistdir, S.buildFileListName(ntuple))
     except Exception as exc:
         continue
 
