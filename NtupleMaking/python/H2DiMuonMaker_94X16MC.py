235abd64389b02ac10626654def5e6d7e1818b3c
malhusse <malhusse@fnal.gov>
2019-06-09 16:23:40 +0200

    update python configs for new ntupling

--- NtupleMaking/python/H2DiMuonMaker_94X16MC.py
+++ NtupleMaking/python/H2DiMuonMaker_94X16MC.py
@@ -1,3 +1,4 @@
+
 import FWCore.ParameterSet.Config as cms
 pt = 27
 
@@ -18,21 +19,23 @@ ntuplemaker_H2DiMuonMaker = cms.EDAnalyzer(
     tagMetFilterResults=cms.untracked.InputTag("TriggerResults", "", "RECO"),
     tagMET=cms.untracked.InputTag("slimmedMETs"),
     tagJets=cms.untracked.InputTag("updatedPatJetsUpdatedJEC"),
-    tagRho = cms.untracked.InputTag("fixedGridRhoFastjetAll"),
+    tagRho=cms.untracked.InputTag("fixedGridRhoFastjetAll"),
     tagGenJets=cms.untracked.InputTag("slimmedGenJets"),
     tagConversions=cms.untracked.InputTag("reducedEgamma:reducedConversions"),
+    tagCands=cms.InputTag("packedPFCandidates"),
 
     rochesterFile=cms.FileInPath("../data/Roccor/RoccoR2016.txt"),
     btagFile=cms.FileInPath("../data/BtagSF/DeepCSV_2016LegacySF_V1.csv"),
     muonIsoFile=cms.FileInPath("../data/MuonSF/RunBCDEF2016_SF_ISO.json"),
     muonIdFile=cms.FileInPath("../data/MuonSF/RunBCDEF2016_SF_ID.json"),
-    muonTrigFile=cms.FileInPath("../data/MuonSF/EfficienciesAndSF_RunBtoF_MuTrig.root"),
+    muonTrigFile=cms.FileInPath(
+        "../data/MuonSF/EfficienciesAndSF_RunBtoF_MuTrig.root"),
 
-    muon_id_sf_wp_num = cms.string("MediumID"),
-    muon_id_sf_wp_den = cms.string("genTracks"),
-    muon_iso_sf_wp_num = cms.string("LooseRelIso"),
-    muon_iso_sf_wp_den = cms.string("MediumID"),
-  
+    muon_id_sf_wp_num=cms.string("MediumID"),
+    muon_id_sf_wp_den=cms.string("genTracks"),
+    muon_iso_sf_wp_num=cms.string("LooseRelIso"),
+    muon_iso_sf_wp_den=cms.string("MediumID"),
+
     #
     #	Meta Data
     #
@@ -41,10 +44,10 @@ ntuplemaker_H2DiMuonMaker = cms.EDAnalyzer(
     triggerNames=cms.untracked.vstring(
         "HLT_IsoMu%d" % pt, "HLT_IsoTkMu%d" % pt),
     metFilterNames=cms.untracked.vstring(["Flag_goodVertices", "Flag_globalSuperTightHalo2016Filter", "Flag_HBHENoiseFilter", "Flag_HBHENoiseIsoFilter",
-                                          "Flag_EcalDeadCellTriggerPrimitiveFilter", "Flag_BadPFMuonFilter" 
-                                          #, "Flag_BadChargedCandidateFilter" as per https://twiki.cern.ch/twiki/bin/view/CMS/MissingETOptionalFiltersRun2
+                                          "Flag_EcalDeadCellTriggerPrimitiveFilter", "Flag_BadPFMuonFilter"
+                                          # , "Flag_BadChargedCandidateFilter" as per https://twiki.cern.ch/twiki/bin/view/CMS/MissingETOptionalFiltersRun2
                                           ]),
-                                          
+
     nMuons=cms.untracked.int32(2),
     isGlobalMuon=cms.untracked.bool(True),
     isStandAloneMuon=cms.untracked.bool(False),
@@ -55,8 +58,12 @@ ntuplemaker_H2DiMuonMaker = cms.EDAnalyzer(
         ["pfDeepCSVJetTags:probb", "pfDeepCSVJetTags:probbb"]),
     tauIDNames=cms.untracked.vstring([""]),
 
+    deepCSVL=cms.untracked.double(0.2217),
+    deepCSVM=cms.untracked.double(0.6321),
+    deepCSVT=cms.untracked.double(0.8953),
+
     #
     #   Some flags
     #
-    useElectrons=cms.untracked.bool(False),
+    useElectrons=cms.untracked.bool(True),
     useTaus=cms.untracked.bool(False))
