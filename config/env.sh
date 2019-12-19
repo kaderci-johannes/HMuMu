9ffc6cfa6f4fe6d8cbf84a809c22e9ec1fd4ff16
malhusse <malhusse@fnal.gov>
2019-12-19 20:47:31 +0100

    fix env script

--- config/env.sh
+++ config/env.sh
@@ -4,7 +4,7 @@
 #	for STANDALONE use it's the build directory or for testing purposes repo
 #export ANALYSISHOME=/afs/cern.ch/work/m/malhusse/private/h2mu/CMSSW_9_4_9_cand2/src/HMuMu
 #export ANALYSISHOME=/afs/cern.ch/work/m/malhusse/private/higgsmumu/h2mu17/CMSSW_9_4_13/src/HMuMu
-export ANALYSISHOME=/afs/cern.ch/work/m/malhusse/private/higgsmumu/h2mu18/CMSSW_10_2_10/src/HMuMu
+export ANALYSISHOME=/afs/cern.ch/work/m/malhusse/private/higgsmumu/h2mu18/CMSSW_10_2_15/src/HMuMu
 #/afs/cern.ch/work/m/malhusse/private/h2mu/CMSSW_9_4_13/src/HMuMu
 #export PYTHONPATH=$ANALYSISHOME:$PYTHONPATH
 echo "ANALYSIS HOME is now at $ANALYSISHOME"
